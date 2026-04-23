2b4d1942871815cf8f03c8ebd7bc486184a1087a
Jarrell <jhouston2882@yahoo.com>
2026-04-23 15:07:17 -0500

    Changes to environment - Skybox, Volume, Lighting

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,10 +4,16 @@ public class CollisionHandler : MonoBehaviour
 {
 
     [SerializeField] GameObject playerDestroyedVfx;
+    GameSceneManager gameSceneManager;
+
+    private void Start(){
+        gameSceneManager = FindFirstObjectByType<GameSceneManager>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         Instantiate(playerDestroyedVfx, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
+        gameSceneManager.ReloadLevel();
     }
 }
